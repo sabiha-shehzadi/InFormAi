06233b977a2633ec6f1e074cfdaaba65bb41f0ac
Sabiha Shehzadi <109547176+sabiha-shehzadi@users.noreply.github.com>
2025-11-07 13:31:24 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Smart Form Automation", layout="wide")
+
+st.sidebar.success("Select a page above.")
+st.title("📄 Smart Form Automation")
+
+st.markdown("""
+Welcome!  
+Choose **Owner** if you want to create & send forms,  
+or **Form** if you’re a colleague filling one out.
+""")
